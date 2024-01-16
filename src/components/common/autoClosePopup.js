29e30c4c8f14bad89cf32a9d36bd68b86f4960d5
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2024-01-17 03:09:49 +0900

    자동닫힘 팝업 초 수정

--- src/components/common/autoClosePopup.js
+++ src/components/common/autoClosePopup.js
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { openPopup } from "../../utils/common";
 import { ErrorTitle, ErrorWrapper } from "../../styles/common/errorStyle";
-
+let to=null;
 const AutoClosePopup = () => {
 
     const dispatch = useDispatch();
-    const {popupMsg, param} = useSelector(state=>state.popup);
+    const {popupMsg, param, isPopupVisible} = useSelector(state=>state.popup);
     useState(()=>{
-        const to = setInterval(() => {
+        to = setInterval(() => {
             clearInterval(to);
+            to=null;
             openPopup(dispatch,{innerView:"", isPopupVisible:false});
-        }, 7000);
+        }, 3000);
     },[])
 
     return(
